1cb52d39556719282991a3279c912004679b50cd
Chaitanya Raut <craut838@gmail.com>
2026-05-22 11:52:37 +0530

    Update productModel.js

--- model/productModel.js
+++ model/productModel.js
@@ -1,35 +1,30 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  sellerId: {
+const paymentSchema = new mongoose.Schema({
+  paymentType: {
+    type: String,
+    enum: ["COD", "Online"],
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  buyerId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, default: "" },
-  type: {
-    type: String,
-    required: true,
-    enum: ["vegetable", "fruit", "flower","Tools","fertilizers"],
-  },
-  specification: [
-    {
-      _id: false,
-      name: { type: String },
-      value: { type: String },
-    },
-  ],
-  image: { type: mongoose.Schema.Types.ObjectId, ref: "Image", default: null },
-
   status: {
     type: String,
-    enum: ["In Stock", "Out of Stock"],
-    default: "In Stock",
+    enum: ["pending", "completed", "failed", "refunded"],
   },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
 });
 
-
-module.exports = mongoose.model("Product", productSchema);
-
+module.exports = mongoose.model("Payment", paymentSchema);
